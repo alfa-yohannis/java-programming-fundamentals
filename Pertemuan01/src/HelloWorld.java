e89cf9f2408cb75b5537097ff541dc2f26a2425d
Alfa Yohannis <alfa.yohannis@gmail.com>
2022-09-21 14:41:13 +0100

    Update .classpath, .project, and 3 more files...

---
+++ Pertemuan01/src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		int a = 3;
+		System.out.println("AWAL");
+		System.out.println(String.valueOf(a));
+		System.out.println("AKHIR");
+
+	}
+}
+
+
